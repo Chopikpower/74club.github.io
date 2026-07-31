f9895c8409dcfb1aaf4382c9ae35310f754dda21
Chopikpower <Caponerj@mail.ru>
2026-07-31 08:11:13 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1397,8 +1397,8 @@ function calculateSymmetricSeatPositions(totalSeats) {
         const angle = startAngle + i * angleStep;
 
         positions.push({
-            x: 50 + 42 * Math.cos(angle),
-            y: 50 + 38 * Math.sin(angle)
+            x: 50 + 44 * Math.cos(angle),
+            y: 50 + 44 * Math.sin(angle)
         });
     }
 
